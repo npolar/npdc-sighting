e10052adbf28b4ec35aca9d464553cb04fbd6296
srldl <siri.uldal@gmail.com>
2016-09-21 10:06:25 +0200

    search fixed except new button link

--- src/sighting-db/search/SightingSearchController.js
+++ src/sighting-db/search/SightingSearchController.js
@@ -8,32 +8,17 @@ var SightingSearchController = function ($scope, $location, $controller, $filter
   $scope.resource = Sighting;
 
 
-  /* npdcAppConfig.search.local.results.detail = (entry) => {
-     let updatedText = NpolarTranslate.translate('Last updated');
-     let r = "  " + updatedText +":";
-     let eventDateText = NpolarTranslate.translate('Event date');
-     let e =  eventDateText +":";
-
-     if (entry.event_date && entry.event_date !== null) {
-         return  e+` ${(entry.event_date.split('T')[0])}` + r+` ${(entry.updated.split('T')[0])}`;
-     } else {
-        return 'No dates';
-     }
-};
-
-
-  npdcAppConfig.cardTitle = "Marine mammal sighting";
 
   //Get title and subtitle
-  npdcAppConfig.search.local.results.title = "title";
-  npdcAppConfig.search.local.results.subtitle = "species"; */
+  npdcAppConfig.search.local.results.title = "species";
+  npdcAppConfig.search.local.results.subtitle = "@placename";
 
 
   let query = function() {
     let defaults = {
       limit: "50",
       sort: "-updated",
-      fields: 'title,event_date,species,habitat,updated,id,locality',
+      fields: 'species,event_date,updated,id,habitat,@placename',
       facets: 'draft'};
 
     let invariants = $scope.security.isAuthenticated() ? {} : {} ;
